cbb4877e25fcb14c1a381e02309d9f97845f267e
Amarakalyan <86305543+Amarakalyan@users.noreply.github.com>
2021-11-10 18:38:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+var noseX=0;
+var noseY=0;
+function preload(){
+clown_nose=loadImage('https://i.postimg.cc/Pq1WzpHQ/Clown-Nose-PNG-Photo-4.png');
+}
+
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    video.size(300,300);
+    
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded()  
+{
+    console.log("PoseNet is Initialized");
+}
+
+function gotPoses(results){
+  if(results.length>0)
+  {
+      console.log(results);
+      console.log("nose x = "+results[0].pose.nose.x);
+      console.log("nose y = "+results[0].pose.nose.y);
+      //noseX=results[0].pose.nose.x;
+      //noseY=results[0].pose.nose.y;
+      noseX=results[0].pose.nose.x-10;
+      noseY=results[0].pose.nose.y-10;
+  }
+}
+
+function draw(){
+    image(video,0,0,300,300);
+    image(clown_nose,noseX,noseY,20,20);
+    //fill(0,0,255);
+    //stroke(0,255,255);
+    //circle(noseX,noseY,20);
+}
+
+function take_snapshot(){
+    save('FilterImage.png');
+}
